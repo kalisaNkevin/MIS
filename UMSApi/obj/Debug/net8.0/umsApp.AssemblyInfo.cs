4876c8ca7720179807f31d4f14ca7a0aedd2ccf0
KEVIN <kevinkalisa84@gmail.com>
2024-04-22 18:32:11 +0200

    initial commit

--- UMSApi/obj/Debug/net8.0/umsApp.AssemblyInfo.cs
+++ UMSApi/obj/Debug/net8.0/umsApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("umsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ccdfe62cfb343c2b9acc28484539181ad743b2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("umsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("umsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
